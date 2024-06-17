0f4eb3eba666e2598f5e6cd2593461a81f79a9cb
sayurusandaru <sayurusadaru12@gmail.com>
2024-06-17 10:13:40 +0530

    Refactor base path handling in Router.php and dbConnection

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/src/Router/auth_route.php';
 require_once __DIR__ . '/src/Router/common_route.php';
 require_once __DIR__ . '/src/Router/driver_route.php';
 require_once __DIR__ . '/src/Router/vehicle_router.php';
-
+require_once __DIR__ . '/src/Utility/cors.php';
 
 use App\Router\Router;
 
