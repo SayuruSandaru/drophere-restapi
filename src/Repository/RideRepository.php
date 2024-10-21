9d5ff79f60dd24b3ea955137f83b87f2eadab9f1
Sayuru Herath <sayurusadaru12@gmail.com>
2024-10-21 10:58:49 +0530

    Refactor: Round the fee to the nearest whole number in RideRepository

--- src/Repository/RideRepository.php
+++ src/Repository/RideRepository.php
@@ -351,12 +351,15 @@ class RideRepository
         $distance = $this->getRoadDistance($pickup, $destination);
         $charge = $this->getFees($vehicleType);
         $fee = $charge['price_per_km'] * $distance;
+        $round = ceil($fee);
         $response = [
-            'fee' => $fee,
+            'fee' => $round,
             'distance' => $distance
         ];
+        
         return $response;
     }
+    
 
 
 
